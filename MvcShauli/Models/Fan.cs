13e18b6db8a4d155154574984a2b351fab589fcd
orlygb25 <orly.gabay25@gmail.com>
2015-09-07 20:03:40 +0300

    Add shauli project

---
+++ MvcShauli/Models/Fan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcShauli.Models
+{
+        public class Fan
+        {
+            public int ID { get; set; }
+            [DisplayName("First name")]
+            public string FirstName { get; set; }
+            [DisplayName("Last name")]
+            public string LastName { get; set; }
+            public Gender Gender { get; set; }
+
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+            public DateTime Birthday { get; set; }
+
+            public int Seniority { get; set; }
+
+            public Fan()
+            {
+                Birthday = DateTime.Now;
+            }
+        }
+
+        public class FanDBContext : DbContext
+        {
+            public DbSet<Fan> fan;
+            public System.Data.Entity.DbSet<MvcShauli.Models.Fan> Fans { get; set; }
+        }
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+    
+}
